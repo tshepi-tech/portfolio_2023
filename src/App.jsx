0e5d9bd981b2f775aef2c5d209e070b6a03cab3b
tshepiso <tshepi.tech@gmail.com>
2023-05-28 08:35:48 +0200

    creates hero with initial css

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./Pages/Home";
+
+export default function App() {
+	return (
+		<div className="App">
+			<Home />
+		</div>
+	);
+}
+
+//use intersection observer
---
+++ src/Sections/Hero.jsx
@@ -0,0 +1,15 @@
+import HeroStyles from "../Styles/HeroStyles.css";
+export default function Hero() {
+	return (
+		<section className="hero">
+			<div className="hero-inner">
+				<h1> Tshepiso Lehutjo </h1>
+				<h2>Full stack developer</h2>
+				<a href="#contact" title="contact">
+					Let's talk
+				</a>
+				<a title="View resume">Resume</a>
+			</div>
+		</section>
+	);
+}
