4f2c4cc461cacef3e2df576feebd7a47324fbb86
tshepiso <tshepi.tech@gmail.com>
2023-05-30 17:23:37 +0200

    toggle icon & theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import AppStyles from "./Styles/AppStyles.css";
+import { useState, useEffect } from "react";
 import Home from "./Pages/Home";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faLightbulb } from "@fortawesome/free-regular-svg-icons";
+import { faMoon } from "@fortawesome/free-regular-svg-icons";
+
+library.add(faLightbulb, faMoon);
 
 export default function App() {
+	const [theme, setTheme] = useState("");
+
+	const toggleTheme = () => {
+		if (theme === "") {
+			setTheme("dark");
+		} else {
+			setTheme("");
+		}
+	};
+
+	useEffect(() => {
+		document.body.className = theme;
+	}, [theme]);
+
 	return (
-		<div className="App">
+		<div className={`${theme} container`}>
+			<button className="toggle" onClick={toggleTheme}>
+				<FontAwesomeIcon icon={theme === "" ? faMoon : faLightbulb} />
+			</button>
 			<Home />
 		</div>
 	);
